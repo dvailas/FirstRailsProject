3627bf734db8754ad567178a102994cedba0ca09
Ubuntu <vagrant@Base-Box>
2018-09-30 20:10:53 +0000

    Added Several Controllers. -Changed index routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 
 Rails.application.routes.draw do
+  get 'songs' => 'songs#index'
+  get 'songs/show'
+  get 'songs/new'
+  get 'songs/edit'
+  get 'albums' => 'albums#index'
+  get 'albums/show'
+  get 'albums/new'
+  get 'albums/edit'
+  get 'genres' => 'genres#index'
+  get 'genres/show'
+  get 'artists' => 'artists#index'
+  get 'artists/show'
+  get 'artists/new'
+  get 'artists/edit'
   get 'welcome/index'
- 
+
   root 'welcome#index'
 end
