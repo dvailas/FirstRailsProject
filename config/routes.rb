c35532615c98093900f0cb0c267d3ed81a3663db
Ubuntu <vagrant@Base-Box>
2018-10-02 14:25:08 +0000

    Changed all lists to use tables instead, revalidated html

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,12 @@ Rails.application.routes.draw do
   get 'result' =>'welcome#result'
   get 'songs' => 'songs#index'
   get 'songs/show'
-  get 'songs/new'
-  get 'songs/edit'
   get 'albums' => 'albums#index'
   get 'albums/show'
-  get 'albums/new'
-  get 'albums/edit'
   get 'genres' => 'genres#index'
   get 'genres/show'
   get 'artists' => 'artists#index'
   get 'artists/show'
-  get 'artists/new'
-  get 'artists/edit'
   get 'welcome/index'
   resources :artists
   resources :songs
