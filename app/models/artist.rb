8b5f4442c2e7d1967387be639cab6911771aa67f
Ubuntu <vagrant@Base-Box>
2018-10-02 04:11:57 +0000

    Cleaned up individual pages.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,11 @@
 class Artist < ApplicationRecord
-  has_many :genre
+  belongs_to :genre
   has_many :songs
   has_many :albums
-
+  #to change to genre_Id
+  #create join table
+    #GenreID
+    #ArtistID
   validates :name, :genre, presence: true
   validates :name, uniqueness: true
   validates :popularity, numericality: {only_integer: true}
